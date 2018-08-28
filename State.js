9ad6d9d3f604b1829bb25c1ddae4b636558cb28f
James P. Fulford <james.patrick.fulford@gmail.com>
2018-08-28 16:16:15 -0400

    Turning off date simulation

--- State.js
+++ State.js
@@ -8,14 +8,14 @@ defaultState = {
     // now: new Date("8/24/2018 14:00:00"),  // Friday,             NOT_SHABBAT
     // now: new Date("8/24/2018 19:16:32"),  // Friday,             NOT_SHABBAT => CANDLELIGHTING
     // now: new Date("8/24/2018 19:22:30"),  // Friday,             CANDLELIGHTING
-    now: new Date("8/24/2018 19:34:30"),  // Friday,             CANDLELIGHTING => SHABBAT
+    // now: new Date("8/24/2018 19:34:30"),  // Friday,             CANDLELIGHTING => SHABBAT
     // now: new Date("8/24/2018 21:00:00"),  // Friday,             SHABBAT
     // now: new Date("8/24/2018 23:59:55"),  // Friday => Saturday, SHABBAT
     // now: new Date("8/25/2018 14:00:00"),  // Saturday,           SHABBAT
     // now: new Date("8/25/2018 20:14:55"),  // Saturday,           SHABBAT => NOT_SHABBAT
     // now: new Date("8/25/2018 21:00:00"),  // Saturday,           NOT_SHABBAT
 
-    // now: new Date(),  // Actual time
+    now: new Date(),  // Actual time
 }
 var userState = createStore(function (state = defaultState, action) {
     if(action.type === "SET_LOCATION") {
