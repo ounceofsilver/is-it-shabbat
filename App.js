50bc5425af85010f2968d004eab54718d4accdac
James P. Fulford <james.patrick.fulford@gmail.com>
2018-11-25 11:44:56 -0500

    Upgraded Expo, fixed App.js linting

--- App.js
+++ App.js
@@ -1,116 +1,110 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-  Text,
-  View,
-  ScrollView
+	Image,
+	Asset,
 } from 'react-native';
 import {
-  Location,
-  Permissions,
-  Font,
-  AppLoading
+	Location,
+	Permissions,
+	Font,
+	AppLoading,
 } from 'expo';
 
-import Router from "./src/Router";
+import { DateTime } from 'luxon';
 
-import ShabbatCheck from './src/components/ShabbatCheck';
-
-import state from "./src/logic/State";
-import Styles from "./src/Styles";
+import state from './src/logic/State';
 
 function cacheFonts(fonts) {
-  return fonts.map(font => Font.loadAsync(font));
+	return fonts.map(font => Font.loadAsync(font));
 }
 
 function cacheImages(images) {
-  return images.map(image => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
+	return images.map((image) => {
+		if (typeof image === 'string') {
+			return Image.prefetch(image);
+		}
+		return Asset.fromModule(image).downloadAsync();
+	});
 }
 
 export default class App extends Component {
+	static async loadAssetsAsync() {
+		const imageAssets = cacheImages([
+			// 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+			// require('./assets/images/circle.jpg'),
+		]);
 
-  state = {
-    isReady: false,
-  };
+		const fontAssets = cacheFonts([
+			{
+				FredokaOne: require('./assets/fonts/FredokaOne.ttf'),
+				fa: require('@expo/vector-icons/fonts/FontAwesome.ttf'),
+			},
+		]);
+		await Promise.all([...imageAssets, ...fontAssets]);
+	}
 
-  async _loadAssetsAsync() {
-    const imageAssets = cacheImages([
-      // 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-      // require('./assets/images/circle.jpg'),
-    ]);
+	state = {
+		isReady: false,
+	};
 
-    const fontAssets = cacheFonts([
-      {
-        'FredokaOne': require('./assets/fonts/FredokaOne.ttf'),
-        fa: require("@expo/vector-icons/fonts/FontAwesome.ttf")
-      }
-    ]);
-    await Promise.all([...imageAssets, ...fontAssets]);
-  }
+	getLocationAsync = async () => {
+		const {
+			status,
+		} = await Permissions.askAsync(Permissions.LOCATION);
+		if (status !== 'granted') {
+			return {
+				// Default location
+				coords: {
+					latitude: 31.776875,
+					longitude: 35.233673,
+				},
 
-  _getLocationAsync = async () => {
-    let {
-      status
-    } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      return {
-        // Default location
-        coords: {
-          latitude: 31.776875,
-          longitude: 35.233673
-        }
+			};
+		}
+		const location = await Location.getCurrentPositionAsync({});
+		console.log('Location received', location.coords.latitude, location.coords.longitude);
+		return location;
+	};
 
-      }
-    } else {
-      let location = await Location.getCurrentPositionAsync({});
-      console.log("Location received", location.coords.latitude, location.coords.longitude);
-      return location;
-    }
-  };
+	render() {
+		const { isReady } = this.state;
+		if (!isReady) {
+			return (
+				<AppLoading
+					startAsync={async () => {
+						this.loadAssetsAsync();
+						console.log('ASYNC APPLOADING LOCATION APP');
+						this.getLocationAsync().then((location) => {
+							state.user.dispatch({
+								type: 'SET_LOCATION',
+								location,
+							});
+						});
+					}}
+					onFinish={() => {
+						state.user.dispatch({
+							type: 'SET_NOW',
+							now: DateTime.local(),
+							// now: new Date("8/22/2018 07:00:00"),  //                 NOT_SHABBAT
+							// now: new Date("8/24/2018 14:00:00"),  // Friday,             NOT_SHABBAT
+							// now: new Date("8/24/2018 19:16:32"),  // Friday,             NOT_SHABBAT => CANDLELIGHTING
+							// now: new Date("8/24/2018 19:22:30"),  // Friday,             CANDLELIGHTING
+							// now: new Date("8/24/2018 19:34:30"),  // Friday,             CANDLELIGHTING => SHABBAT
+							// now: new Date("8/24/2018 21:00:00"),  // Friday,             SHABBAT
+							// now: new Date("8/24/2018 23:59:55"),  // Friday => Saturday, SHABBAT
+							// now: new Date("8/25/2018 14:00:00"),  // Saturday,           SHABBAT
+							// now: new Date("8/25/2018 20:14:55"),  // Saturday,           SHABBAT => NOT_SHABBAT
+							// now: new Date("8/25/2018 21:00:00"),  // Saturday,           NOT_SHABBAT
+						});
+						// console.log((new Date("8/24/2018 19:16:36")) - (new Date("8/24/2018 19:34:36")));
 
-  render() {
-    if (!this.state.isReady) {
-      return (
-        <AppLoading
-          startAsync={async () => {
-            console.log("ASYNC APPLOADING ASSETS APP");
-            this._loadAssetsAsync();
-            console.log("ASYNC APPLOADING LOCATION APP");
-            this._getLocationAsync().then(location => {
-              state.user.dispatch({
-                type: "SET_LOCATION",
-                location,  // JS shorthand for location: location
-              });
-            });
-          }}
-          onFinish={() => {
-            state.user.dispatch({
-              type: "SET_NOW",
-              now: new Date(),
-              // now: new Date("8/22/2018 07:00:00"),  //                     NOT_SHABBAT
-              // now: new Date("8/24/2018 14:00:00"),  // Friday,             NOT_SHABBAT
-              // now: new Date("8/24/2018 19:16:32"),  // Friday,             NOT_SHABBAT => CANDLELIGHTING
-              // now: new Date("8/24/2018 19:22:30"),  // Friday,             CANDLELIGHTING
-              // now: new Date("8/24/2018 19:34:30"),  // Friday,             CANDLELIGHTING => SHABBAT
-              // now: new Date("8/24/2018 21:00:00"),  // Friday,             SHABBAT
-              // now: new Date("8/24/2018 23:59:55"),  // Friday => Saturday, SHABBAT
-              // now: new Date("8/25/2018 14:00:00"),  // Saturday,           SHABBAT
-              // now: new Date("8/25/2018 20:14:55"),  // Saturday,           SHABBAT => NOT_SHABBAT
-              // now: new Date("8/25/2018 21:00:00"),  // Saturday,           NOT_SHABBAT
-            });
-            // console.log((new Date("8/24/2018 19:16:36")) - (new Date("8/24/2018 19:34:36")));
-
-            this.setState({ isReady: true });
-          }}
-          onError={console.warn}
-        />
-      )
-    }
-    return this.state.isReady && (<Router />);
-  }
+						this.setState({ isReady: true });
+					}}
+					onError={console.warn}
+				/>
+			);
+		}
+		return isReady;
+		// return isReady && (<Router />);
+	}
 }
