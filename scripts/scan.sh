099c073c7df5d1f1be3cdcf40332a73f6921ab18
James Fulford <james.fulford@outlook.com>
2019-01-01 02:47:50 -0500

    Minor scan prop adjustments

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,7 +1,8 @@
 sonar-scanner \
 	-Dsonar.projectKey=ounceofsilver_isitshabbat \
 	-Dsonar.organization=ounceofsilver \
-	-Dsonar.sources=. \
+	-Dsonar.sources=./src \
 	-Dsonar.host.url=https://sonarcloud.io \
 	-Dsonar.login=$SONARCLOUD_ISITSHABBATAPP \
-	-Dsonar.javascript.lcov.reportPaths=coverage/lcov.info
+	-Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+	-Dsonar.coverage.exclusions=**/*.spec.js
