c5d3a7e3857685634380e30c72bbbfd6ec415b31
James Fulford <james.fulford@outlook.com>
2019-05-03 00:03:20 -0400

    fix(holidays): Ignore omer holidays

--- src/utilities/holidayFiltering.js
+++ src/utilities/holidayFiltering.js
@@ -1,6 +1,7 @@
 export default function (holidays, now) {
 	return holidays
 		.filter(h => h.date > now)
+		.filter(h => h.category !== 'omer')
 		.sort((h1, h2) => h1.date.diff(h2.date).shiftTo('milliseconds').milliseconds)
 		.slice(0, 3);
 }
