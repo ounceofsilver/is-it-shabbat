a4f68d148047241bb27f9b97d1ad33f9f0da0eb9
James Fulford <james.fulford@outlook.com>
2019-06-10 21:27:02 -0400

    WIP(app): Refactoring time, App.tsx (useKeepAwake)

--- src/initialization.ts
+++ src/initialization.ts
@@ -7,8 +7,7 @@ import { action, state, utilities } from 'is-it-shabbat-core';
 import { Image } from 'react-native';
 
 import getLocation from './components/location/getLocation';
-
-const { DateTime } = utilities;
+import { getTime } from './time';
 
 //
 // Assets loading
@@ -41,69 +40,10 @@ export default async () => {
 	await Promise.all([
 		loadAssetsAsync(),
 		getLocation().then((location) => {
-			const act = action.initialize(
-				DateTime.local(),
-				//
-				//
-				// Simulate different times
-				//
-				//
-
-				// Done at (43, -71)
-				//
-				// 2 weeks before Rosh Hashana
-				//
-
-				// Weekday
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 22, hour: 7, minute: 0, second: 0 }),
-
-				// Friday, pre-candlelighting
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 14, minute: 0, second: 0 }),
-
-				// Candlelighting Crossover
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 19, minute: 18, second: 10 }),
-
-				// Candlelighting
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 19, minute: 22, second: 30 }),
-
-				// Shabbat crossover
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 19, minute: 36, second: 10 }),
-
-				// Friday Shabbat
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 21, minute: 0, second: 0 }),
-
-				// Friday Saturday crossover
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 24, hour: 23, minute: 59, second: 55 }),
-
-				// Saturday Shabbat
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 25, hour: 14, minute: 0, second: 0 }),
-
-				// Havdala
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 25, hour: 20, minute: 16, second: 32 }),
-
-				// Saturday Not Shabbat
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 8, day: 25, hour: 21, minute: 0, second: 0 }),
-
-				//
-				// Hannukah
-				//
-
-				// Holiday crossover
-				// tslint:disable-next-line: max-line-length
-				// DateTime.fromObject({ zone: 'America/New_York', year: 2018, month: 12, day: 2, hour: 16, minute: 11, second: 44 }),
+			state.dispatch(action.initialize(
+				getTime(),
 				location,
-			);
-			state.dispatch(act);
+			));
 		}),
 	]);
 };
