98f53d92f3f8dbe69de25b06a7365afcfde60bfd
James Fulford <james.fulford@outlook.com>
2019-09-02 23:59:21 -0400

    feat(*): Integrate error handling

--- src/app/components/location/getLocation.ts
+++ src/app/components/location/getLocation.ts
@@ -1,8 +1,10 @@
 import { Location, Permissions } from 'expo';
 
 import { ILocation } from '../../../core/models/config';
+import { setError } from '../../../core/store/use/error';
+import store from '../../store';
 
-const defaultLocation = {
+const defaultLocation: ILocation = {
 	// TODO(james.fulford): get location by IP
 	coords: {
 		latitude: 31.776875,
@@ -14,12 +16,13 @@ export default async (): Promise<ILocation> => {
 	try {
 		const { status } = await Permissions.askAsync(Permissions.LOCATION);
 		if (status !== 'granted') {
+			store.dispatch(setError('location.denied'));
 			return Promise.resolve(defaultLocation);
 		}
 		const location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Lowest });
 		return Promise.resolve(location);
 	} catch (e) {
-		// console.error(e);
+		store.dispatch(setError('location.unavailable'));
 		return Promise.resolve(defaultLocation);
 	}
 };
