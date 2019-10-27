01b9b2ba4a9fb246a46beedf9c4024b4113625a0
James Fulford <james.fulford@outlook.com>
2019-10-27 18:46:56 -0400

    fix(selectors): typescript build difference fix for iOS

--- src/core/store/config/selectors.ts
+++ src/core/store/config/selectors.ts
@@ -1,7 +1,12 @@
-import * as lookup from 'tz-lookup';
+import * as _lookup from 'tz-lookup';
 
 import { AppState } from '..';
 import { ILocation } from './types';
+
+// In prod build, this _lookup is a function.
+// In tests and web version, _lookup.lookup is the function.
+// TypeScript has its moments. This is not one of the good ones.
+const lookup: (lat: number, long: number) => ReturnType<_lookup> = _lookup.default || _lookup;
 
 export function getLocation(state: AppState): ILocation | undefined {
 	return state.config.location;
