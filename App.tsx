a81137c486288dce26eb4eafa0c67a1bce50d1ef
James Fulford <james.fulford@outlook.com>
2023-02-07 17:04:29 -0500

    service worker with offline mode (faster reloads)

--- App.tsx
+++ App.tsx
@@ -1,2 +1,8 @@
 import { App } from './src/app/container';
 export default App;
+
+import * as serviceWorkerRegistration from "./src/serviceWorkerRegistration";
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
